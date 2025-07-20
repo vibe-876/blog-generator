bb94401662d5f1d39d33dd22f4c21252f190dea5
cam <cameronayres123@gmail.com>
2025-07-20 20:49:55 +0100

    Can now do VERY basic translations (doesn't save them yet).

--- src/blog_generator/core.clj
+++ src/blog_generator/core.clj
@@ -1,7 +1,34 @@
 (ns blog-generator.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+
+(defn trans-markup-to-html
+  "Maps a line of markup into a line of HTML."
+  [raw-line]
+  (cond (is-title raw-line) (->> (rest raw-line)
+                                 (apply str)
+                                 (#(taggify "h1" %)))
+        
+        :else (taggify "p" raw-line)))
+
+(defn is-title
+  "Is this line a title?
+  This should probably be a macro."
+  [raw-line]
+  (= (first raw-line) \#))
+
+(defn taggify
+  "Enter data between two tags.
+  (taggify a b c) becomes \"<a c>b</a>\"."
+  [tag-name data & extra-data]
+  (apply str ["<" tag-name ">" data "</" tag-name ">"]))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "Program entry point."
+  [markup-doc & args]
+  (->> (slurp markup-doc)
+       (str/split-lines)
+       (map trans-markup-to-html)
+       (apply str)))
