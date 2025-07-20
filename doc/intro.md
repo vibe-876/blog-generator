7db22f4d5c352efb0030f5c30fdd2bc506309c6c
cam <cameronayres123@gmail.com>
2025-07-20 19:54:26 +0100

    Initial commit (lein new app blog-generator).

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to blog-generator
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/blog_generator/core_test.clj
@@ -0,0 +1,7 @@
+(ns blog-generator.core-test
+  (:require [clojure.test :refer :all]
+            [blog-generator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
