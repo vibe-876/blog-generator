644629162d4d4478ddd85470691adc99a55e7b5c
cam <cameronayres123@gmail.com>
2025-07-29 00:22:09 +0100

    Added a very basic emacs major mode for camarkup.

---
+++ camarkup-mode.el
@@ -0,0 +1,34 @@
+;; Get it, Cam's Markup, camarkup. Hilarious, I know.
+;; I didn't want to risk messing up someone's preexisting
+;; markup-mode (I'm assuming that exists somewhere in the ether).
+
+
+(defun camarkup-insert-link ()
+  "Insert a camarkup style link at the
+current position of the cursor.
+
+This should only be called interactively."
+  (interactive)
+  (let ((address (read-string "U.R.I.: "))
+	(text (read-string "Text to display: ")))
+    (insert (concat "!" text "@" address "\n"))))
+
+
+(defvar-keymap camarkup-mode-map
+  :doc "A keymap for `camarkup-mode'."
+  :parent text-mode-map
+  :repeat t
+  "C-c i" #'camarkup-insert-link)
+
+(defcustom camarkup-mode-hook nil
+  "Hook for `camarkup-mode'."
+  :type 'hook)
+
+(define-derived-mode camarkup-mode text-mode "camarkup"
+  "Major mode for editing a specific kinda markup.
+\\{camarkup-mode-map}
+See https://github.com/vibe-876/blog-generator for
+more details."
+  (use-local-map camarkup-mode-map))
+
+(add-to-list 'auto-mode-alist '("\\.cmd$" . camarkup-mode))
