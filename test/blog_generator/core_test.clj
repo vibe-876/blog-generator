7ce18cae7655ad5b9084c24748464a0eb2b0f5e6
cam <cameronayres123@gmail.com>
2025-07-29 14:21:09 +0100

    Wrote some more tests.

--- test/blog_generator/core_test.clj
+++ test/blog_generator/core_test.clj
@@ -3,6 +3,21 @@
             [blog-generator.core :refer :all]))
 
 (deftest trans-p
-  (testing "Are paragraphs translated properly"
-    (is (= (blog-generator.core/taggify "p" "class=\"paragraph\"" "Hello, World :3 !")
+  (testing "Are paragraphs translated properly?"
+    (is (= (taggify "p" "class=\"paragraph\"" "Hello, World :3 !")
            "<p class=\"paragraph\">Hello, World :3 !</p>"))))
+
+(deftest div
+  (testing "Are divs (containing things) translated correctly?"
+    (is (= (map trans-markup-to-html ["~code"
+                                                     "(+ 1 2"
+                                                     "     3)"
+                                                     ";"])
+           ["<div class=\"code\">"
+            "<p >(+ 1 2</p>"
+            "<p >     3)</p>"
+            "</div>"]))))
+
+(deftest link-detecting
+  (testing "Are links being detected properly?"
+    (is (is-link? "!test@wow"))))
