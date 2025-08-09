8f2f1cb24a2909cf7a5a4a32bc7d9fc754687643
cam <cameronayres123@gmail.com>
2025-08-09 01:39:51 +0100

    Can now fully do HTML.

--- src/blog_generator/html.clj
+++ src/blog_generator/html.clj
@@ -7,7 +7,9 @@
   "Translate from the internal representation into
   HTML. Just a special case of trans-ir-xml."
   [ast]
-  (let [html-table {:body "body"
+  (let [head (apply hash-map (first ast))
+        body (apply hash-map (second ast))
+        html-table {:body "body"
                     :paragraph "p"
                     :link "a"
                     :head "head"
@@ -16,5 +18,6 @@
                     :word "p"}]
     
     (str "<html>"
-         (xml/trans-ir-xml html-table ast)
+         (xml/trans-ir-xml html-table head)
+         (xml/trans-ir-xml html-table body)
          "</html>")))
