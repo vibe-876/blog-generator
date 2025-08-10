2a62cbaf4a222b163efe79b9ec50cc6e6f0a1575
cam <cameronayres123@gmail.com>
2025-08-10 20:14:12 +0100

    Changed license from EPLv2 to GPLv3.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject blog-generator "0.1-SNAPSHOT"
   :description "A simple camarkup (a markup language) to HTML translator."
   :url "https://github.com/vibe-876/blog-generator"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "GPLv3"
+            :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot blog-generator.core
   :target-path "target/%s"
